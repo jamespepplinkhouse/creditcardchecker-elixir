f825f9722c08d7b10279ff7426720f79edda1ef7
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2016-03-25 19:34:52 +1000

    Removed guard clauses and added cleanse() function to support both strings and integers

--- lib/creditcard.ex
+++ lib/creditcard.ex
@@ -6,19 +6,13 @@ defmodule Creditcard do
   @doc ~S"""
     Validates a credit card number using the Luhn algorithm cc
   """
-  def card_number_is_valid?(card_number) when is_integer(card_number) do
-    0 == Integer.digits(card_number)
-      |> Enum.reverse
-      |> Enum.chunk(2, 2, [0])
-      |> Enum.reduce(0, fn([odd, even], sum) -> Enum.sum([sum, odd | Integer.digits(even*2)]) end)
-      |> rem(10)
-  end
-
-  def card_number_is_valid?(card_number) when is_binary(card_number) do
-    {card_number_int, _} = String.replace(card_number, ~r/[^0-9]/, "")
-      |> Integer.parse
-
-      card_number_is_valid?(card_number_int)
+  def card_number_is_valid?(card_number) do
+      0 == cleanse(card_number)
+        |> Integer.digits
+        |> Enum.reverse
+        |> Enum.chunk(2, 2, [0])
+        |> Enum.reduce(0, fn([odd, even], sum) -> Enum.sum([sum, odd | Integer.digits(even*2)]) end)
+        |> rem(10)
   end
 
   @doc ~S"""
@@ -26,6 +20,14 @@ defmodule Creditcard do
   """
   def determine_card_type(card_number) when is_integer(card_number) do
 
+  end
+
+  defp cleanse(card_number) do
+    if (is_binary(card_number)) do
+      {card_number, _} = String.replace(card_number, ~r/[^0-9]/, "")
+        |> Integer.parse
+    end
+    card_number
   end
 
 end
