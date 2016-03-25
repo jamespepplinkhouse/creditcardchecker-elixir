c1b51cbe873c5f27b332d176819c08cd986ae088
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2016-03-25 18:26:38 +1000

    ...

--- lib/creditcard.ex
+++ lib/creditcard.ex
@@ -1,51 +1,31 @@
 defmodule Creditcard do
+  @moduledoc """
+    Validates a credit card number and identifies the card type
+  """
 
   @doc ~S"""
-    Validates a credit card number using the Luhn algorithm
-
-    ## Examples
-
-        iex> Creditcard.validate?(4111111111111111)
-        :true
-
-        iex> Creditcard.validate?("4111111111111111")
-        :true
-
-        iex> Creditcard.validate?(4111111111111)
-        :false
-
-        iex> Creditcard.validate?(4012888888881881)
-        :true
-
-        iex> Creditcard.validate?(378282246310005)
-        :true
-
-        iex> Creditcard.validate?(6011111111111117)
-        :true
-
-        iex> Creditcard.validate?(5105105105105100)
-        :true
-
-        iex> Creditcard.validate?("5105 1051 0510 5106")
-        :false
-
-        iex> Creditcard.validate?(9111111111111111)
-        :false
-
+    Validates a credit card number using the Luhn algorithm cc
   """
-  def validate?(cc) when is_integer(cc) do
-    0 == Integer.digits(cc)
+  def card_number_is_valid?(card_number) when is_integer(card_number) do
+    0 == Integer.digits(card_number)
       |> Enum.reverse
       |> Enum.chunk(2, 2, [0])
       |> Enum.reduce(0, fn([odd, even], sum) -> Enum.sum([sum, odd | Integer.digits(even*2)]) end)
       |> rem(10)
   end
 
-  def validate?(cc) when is_binary(cc) do
-    {cc_num, _} = String.replace(cc, ~r/[^0-9]/, "")
+  def card_number_is_valid?(card_number) when is_binary(card_number) do
+    {card_number_int, _} = String.replace(card_number, ~r/[^0-9]/, "")
       |> Integer.parse
 
-      validate?(cc_num)
+      card_number_is_valid?(card_number_int)
+  end
+
+  @doc ~S"""
+    Returns a string representing the card type
+  """
+  def determine_card_type(card_number) when is_integer(card_number) do
+
   end
 
 end
--- test/creditcard_test.exs
+++ test/creditcard_test.exs
@@ -2,7 +2,20 @@ defmodule CreditcardTest do
   use ExUnit.Case
   doctest Creditcard
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "can validate credit card number as integer" do
+    assert Creditcard.card_number_is_valid?(4111111111111111) == :true
+    assert Creditcard.card_number_is_valid?(4111111111111111) == :true
+    assert Creditcard.card_number_is_valid?(4111111111111) == :false
+    assert Creditcard.card_number_is_valid?(4012888888881881) == :true
+    assert Creditcard.card_number_is_valid?(378282246310005) == :true
+    assert Creditcard.card_number_is_valid?(6011111111111117) == :true
+    assert Creditcard.card_number_is_valid?(5105105105105100) == :true
+    assert Creditcard.card_number_is_valid?(9111111111111111) == :false
+  end
+
+  test "can validate credit card number as binary" do
+    assert Creditcard.card_number_is_valid?("4111111111111111") == :true
+    assert Creditcard.card_number_is_valid?("5105 1051 0510 5106") == :false
+    assert Creditcard.card_number_is_valid?("411!111$1111#111##111") == :true
   end
 end
