d361f67ec08a1a0777c8574551b99304f5f9781b
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2016-03-26 20:02:08 +1000

    Added CreditcardChecker module which will act as the "app" and do IO

---
+++ lib/creditcard_checker.ex
@@ -0,0 +1,3 @@
+defmodule CreditcardChecker do
+  
+end
---
+++ test/creditcard_checker_test.exs
@@ -0,0 +1,8 @@
+defmodule CreditcardCheckerTest do
+  use ExUnit.Case
+  doctest CreditcardChecker
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
